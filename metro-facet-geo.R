8cbaacf109e29cb07b60d7abd6d589e96a8d1907
mr_ <njgriffiths@gmail.com>
2019-03-18 22:09:30 -0700

    added source map link

--- metro-facet-geo.R
+++ metro-facet-geo.R
@@ -1,5 +1,6 @@
 library(geofacet)
 
+# Metro Vancouver
 metro_van_grid <- data.frame(
   row = c(1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5),
   col = c(5, 2, 6, 1, 3, 4, 5, 2, 4, 5, 2, 8, 7, 6, 3, 7, 8, 5, 3, 6, 4, 3, 6),
@@ -8,3 +9,5 @@ metro_van_grid <- data.frame(
   stringsAsFactors = FALSE
 )
 geofacet::grid_preview(metro_van_grid)
+
+# Source map: http://www.metrovancouver.org/about/Pages/default.aspx
